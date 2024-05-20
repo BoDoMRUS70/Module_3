882e440f430b7adc3ca8a0468d0210a689b013f4
Bodomrus70@mail.ru <Bodomrus70@mail.ru>
2024-05-21 02:31:40 +0700

    Комит

--- Module_3/Program.cs
+++ Module_3/Program.cs
@@ -8,13 +8,17 @@ class Program
 
     static void Main(string[] args)
     {
-        string MyName;
 
-        MyName = "Dima";
+        string MyName = "Jane";
+        byte MyAge = 27;
+        bool HaveIApet = true;
+        double MyShoeSize = 37.5;
 
-        Console.WriteLine(MyName);
+        Console.WriteLine("My name is " + MyName);
+        Console.WriteLine("MyAge " + MyAge);
+        Console.WriteLine("Do I have a pet? " + HaveIApet);
+        Console.WriteLine("My shoe size is " + MyShoeSize);
 
-        Console.ReadKey();
     }
-
 }
+
